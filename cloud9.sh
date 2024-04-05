62b1badaee62e2f139dad6f8e26671b578d50696
Dong <dongdong2yang@gmail.com>
2024-04-05 13:37:28 +0800

    Update cloud9.sh with authorize access to EKS

--- cloud9.sh
+++ cloud9.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+echo "install  required tools \n"
 sudo yum install -y bash-completion moreutils jq
 
+echo "install kubectl,eksctl \n"
 sudo curl --silent --location -o /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 sudo chmod +x /usr/bin/kubectl
 sudo -H -u ec2-user bash -c "kubectl completion bash >>  ~/.bash_completion"
@@ -20,20 +22,16 @@ sudo tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
 
 sudo -H -u ec2-user bash -c "eksctl completion bash >> ~/.bash_completion"
-
+echo "install  helm \n"
 sudo curl --silent --location "https://get.helm.sh/helm-v3.10.1-linux-amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/linux-amd64/helm /usr/bin
 sudo chmod +x /usr/bin/helm
 
-cat << EOF > /home/ec2-user/.bashrc.d/env.bash
-export EKS_CLUSTER_NAME=flink-on-eks
-export ACCOUNTID=${ACCOUNTID}
-export AWS_DEFAULT_REGION=${REGION}
-export KARPENTER_VERSION="v0.27.1"
-export KARPENTER_IAM_ROLE_ARN="arn:aws:iam::${ACCOUNTID}:role/${EKS_CLUSTER_NAME}-karpenter"
+echo "Configure cloud9 to access the EKS cluster"
+export ARN=$(aws sts get-caller-identity --output text --query Arn) 
+export CLUSTER_NAME="eks-cluster-test"
+aws eks create-access-entry --cluster-name $CLUSTER_NAME --principal-arn $ARN --type Standard
+aws eks associate-access-policy --cluster-name $CLUSTER_NAME --principal-arn $ARN --policy-arn arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy
 
-aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME}
-EOF
 
-curl -Lo ec2-instance-selector https://github.com/aws/amazon-ec2-instance-selector/releases/download/v2.4.1/ec2-instance-selector-`uname | tr '[:upper:]' '[:lower:]'`-amd64 && chmod +x ec2-instance-selector
-sudo mv ec2-instance-selector /usr/local/bin/
+echo "Configuring Cloud9 Done \n"
